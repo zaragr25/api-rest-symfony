489a90f58e4bcad84a45c250cf81736df518e807
unknown <zara.gr25912@gmail.com>
2019-11-14 14:55:50 -0600

    Test2

--- public/index.php
+++ public/index.php
@@ -24,4 +24,4 @@ $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
-
+$kernel->terminate($request, $response);
